25e5cdadda0d9ab9114104aa9a875e8183211b48
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2024-05-01 20:13:19 +0530

    Update Header.js

---
+++ workday/src/components/Layout/Header.js
@@ -0,0 +1,109 @@
+// Header.js
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Divider,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,TextField
+} from "@mui/material";
+
+
+
+import MenuIcon from "@mui/icons-material/Menu";
+
+import "../../styles/HeaderStyles.css";
+
+const Header = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [openLogoutDialog, setOpenLogoutDialog] = useState(false);
+ 
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  
+  const handleCloseLogoutDialog = () => {
+    setOpenLogoutDialog(false);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{  textAlign: "center" }}>
+      <Typography
+        color={"goldenrod"}
+        variant="h6"
+       
+        component="div"
+        sx={{ flexGrow: 1, my: 2 }}
+      >
+    
+      </Typography>
+      <Divider />
+     
+    </Box>
+  );
+
+  return (
+    <>
+      <Box>
+        <AppBar component={"nav"} sx={{ bgcolor: "white" }}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              sx={{
+                mr: 2,
+                display: { sm: "none" },
+              }}
+              onClick={handleDrawerToggle}
+              style={{ color: 'black' }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              color={"goldenrod"}
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            >
+            
+            </Typography>
+           
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: "240px",
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box>
+          <Toolbar />
+        </Box>
+      </Box>
+
+      
+    </>
+  );
+};
+
+export default Header;
+ 
