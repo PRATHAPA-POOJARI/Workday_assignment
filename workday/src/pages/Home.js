b37769405d26924f67b7658df813b8c679eb0dee
prathap poojari <72817845+PRATHAPA-POOJARI@users.noreply.github.com>
2024-05-02 20:12:11 +0530

    Update Home.js

--- workday/src/pages/Home.js
+++ workday/src/pages/Home.js
@@ -2,21 +2,32 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout/Layout';
 import JobCard from '../components/JobCard/JobCard';
-import axios from 'axios';
 import FilterPanel from '../components/FilterPanel/FilterPanel';
 
 const Home = () => {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const body = JSON.stringify({
+      "limit": 10,
+      "offset": 0
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body
+    };
+
     const fetchJobs = async () => {
       try {
-        const response = await axios.post("https://api.weekday.technology/adhoc/getSampleJdJSON", {
-          limit: 10,
-          offset: 0
-        });
-        if (response.data.jobs) {
-          setJobs(response.data.jobs);
+        const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+        const data = await response.json();
+        if (data.jobs) {
+          setJobs(data.jobs);
         }
       } catch (error) {
         console.error(error);
